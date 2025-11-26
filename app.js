6b83528d639c301ef428b15355a65b5db3a59289
ElMaxi-bit <mleonle@utem.cl>
2025-11-26 15:49:26 -0300

    primera version

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const ejemploRoutes = require("./src/routes/ejemplo.routes");
+app.use("/api/ejemplo", ejemploRoutes);
+
+const dbTestRoutes = require("./src/routes/db.routes");
+app.use("/api/db-test", dbTestRoutes);
+
+module.exports = app;
